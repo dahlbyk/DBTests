3dd24cedcfee4de53771f2a7d277387a61fade4a
Keith Dahlby <dahlbyk@gmail.com>
2011-10-29 00:13:31 -0500

    Add AdventureWorksLT test

---
+++ src/AdventureWorksTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace DBTests
+{
+    [TestFixture]
+    public class AdventureWorksTests : TestBase
+    {
+        protected override int CommandTimeout
+        {
+            get { return 60; }
+        }
+
+        protected override string ConnectionString
+        {
+            get { return "Data Source=.;Initial Catalog=AdventureWorksLT;Integrated Security=true"; }
+        }
+
+        protected override string TestSchema
+        {
+            get { return "Test"; }
+        }
+    }
+}
